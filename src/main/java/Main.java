f7a9f840655366e025da26bfbbfa2355cd4ce94f
Dulaj Atapattu <data1435@corp.sysco.com>
2020-03-22 19:16:28 +0530

    Add more comments

--- src/main/java/Main.java
+++ src/main/java/Main.java
@@ -6,16 +6,28 @@ public class Main {
         createAndClone();
     }
 
+    /**
+     * Sample method to launch original instance
+     */
     public static void createOriginalInstance() {
         String ip = new OriginalEC2Launcher().launch(Configs.ORIGINAL_INSTANCE_NAME);
         System.out.println("Original instance launched. IP: " + ip);
     }
 
+    /**
+     * Sample method to launch worker instance using an AMI
+     */
     public static void createWorkerInstance() {
         String ip = new WorkerEC2Launcher().launch(Configs.WORKER_INSTANCE_NAME, Configs.WORKER_INSTANCE_AMI_ID);
         System.out.println("Original instance launched. IP: " + ip);
     }
 
+    /**
+     * Sample method to create original instance,
+     * create AMI for original instance and
+     * launch a worker instance using above created AMI.
+     * All is automated.
+     */
     public static void createAndClone() {
         String originalInstanceIP = new OriginalEC2Launcher().launch(Configs.ORIGINAL_INSTANCE_NAME);
         System.out.println("Original instance launched. IP: " + originalInstanceIP);
